852be05d7ef391a7b2d3b34fa2ae533ae1d6f999
liyuzhe <banyebushui@gmail.com>
2025-12-06 20:59:37 +0800

    feat: 在详情页应用栏显示完整的种子名称并优化下载器的浮动通知。

--- lib/pages/torrent_detail_page.dart
+++ lib/pages/torrent_detail_page.dart
@@ -1969,9 +1969,7 @@ class _TorrentDetailPageState extends State<TorrentDetailPage> {
       child: Scaffold(
       appBar: AppBar(
         title: SelectableText(
-          widget.torrentItem.name.length > 50
-              ? '${widget.torrentItem.name.substring(0, 50)}...'
-              : widget.torrentItem.name,
+          widget.torrentItem.name,
           style: TextStyle(
             fontSize: 16,
             color: Theme.of(context).brightness == Brightness.light
@@ -2065,6 +2063,7 @@ class _TorrentDetailPageState extends State<TorrentDetailPage> {
             ),
       floatingActionButton: Column(
         mainAxisAlignment: MainAxisAlignment.end,
+          mainAxisSize: MainAxisSize.min,
         children: [
           // 收藏按钮
           FloatingActionButton(
